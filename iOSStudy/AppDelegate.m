473c4e2554afb841265970a7a3572d02b3dcbd12
985892345 <guo985892345@foxmail.com>
2023-08-09 00:53:30 +0800

    :tada: iOS学习

--- iOSStudy/AppDelegate.m
+++ iOSStudy/AppDelegate.m
@@ -6,6 +6,7 @@
 //
 
 #import "AppDelegate.h"
+#import "ViewController.h"
 
 @interface AppDelegate ()
 
@@ -16,25 +17,21 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    
+    // 创建 UIWindow
+    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+    // 设置 UIWindow 的背景颜色
+    self.window.backgroundColor = UIColor.whiteColor;
+    
+    // 创建 HomeViewController
+    ViewController *homeVC = [[ViewController alloc] init];
+    // 设置 rootViewController
+    self.window.rootViewController = homeVC;
+    
+    // 让 window 成为 keyWindow (主窗口)，并且保持可见
+    [self.window makeKeyAndVisible];
+    
     return YES;
 }
-
-
-#pragma mark - UISceneSession lifecycle
-
-
-- (UISceneConfiguration *)application:(UIApplication *)application configurationForConnectingSceneSession:(UISceneSession *)connectingSceneSession options:(UISceneConnectionOptions *)options {
-    // Called when a new scene session is being created.
-    // Use this method to select a configuration to create the new scene with.
-    return [[UISceneConfiguration alloc] initWithName:@"Default Configuration" sessionRole:connectingSceneSession.role];
-}
-
-
-- (void)application:(UIApplication *)application didDiscardSceneSessions:(NSSet<UISceneSession *> *)sceneSessions {
-    // Called when the user discards a scene session.
-    // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-    // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
-}
-
 
 @end
--- iOSStudy/ViewController.m
+++ iOSStudy/ViewController.m
@@ -5,9 +5,13 @@
 //  Created by 郭祥瑞 on 2023/8/8.
 //
 
+// https://www.bilibili.com/video/BV1ce4y1272i?p=5&vd_source=6f25bca34bee883261bca409c52a066e
+
 #import "ViewController.h"
 
 @interface ViewController ()
+
+@property(nonatomic,strong) UILabel *label2;
 
 @end
 
@@ -16,7 +20,33 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    
+    // 设置 View 背景颜色
+    self.view.backgroundColor = UIColor.blueColor;
+    
+    // 文本框
+    UILabel *label = [[UILabel alloc] init];
+    // x, y, width, height
+    label.frame = CGRectMake(146, 400, 200, 30); // iOS 仍然以向右为 x 轴，向下为 y 轴
+    label.text = @"Hello World!";
+    label.textColor = UIColor.redColor;
+    label.font = [UIFont boldSystemFontOfSize:20];
+    [self.view addSubview:label];
+    
+    // 添加懒加载的 Label
+    [self.view addSubview:self.label2];
 }
 
+#pragma mark - 懒加载
+
+- (UILabel *)label2 {
+    if (!_label2) {
+        _label2 = [[UILabel alloc] initWithFrame:CGRectMake(90, 450, 220, 60)];
+        _label2.text = @"Hello World!";
+        _label2.font = [UIFont boldSystemFontOfSize:40];
+        _label2.backgroundColor = UIColor.yellowColor;
+    }
+    return _label2;
+}
 
 @end
